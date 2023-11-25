5de94f66aa22436b9bdd34cadedbb022f0316a82
MorTal <baigabdulrehman21@gmail.com>
2023-11-26 00:04:30 +0500

    Camera Functionality with tflite

---
+++ lib/src/features/authentication/controllers/camera_controller.dart
@@ -0,0 +1,89 @@
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+
+class ScanController extends GetxController {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void onInit() {
+    super.onInit();
+    initCamera();
+    initTFLite();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    cameraController.dispose();
+    //Tflite.close();
+  }
+
+  late CameraController cameraController;
+  late List<CameraDescription> cameras;
+
+  var isCameraInitialized = false.obs;
+  var cameraCount = 0;
+
+  initCamera() async {
+    if (await Permission.camera.request().isGranted) {
+      cameras = await availableCameras();
+
+      cameraController = CameraController(cameras[1], ResolutionPreset.max,
+      );
+
+      await cameraController.initialize().then((value) {
+        cameraController.startImageStream((image) {
+          cameraCount++;
+          if (cameraCount % 10 == 0) {
+            cameraCount = 0;
+          }
+          update();
+          objectDetector(image);
+        });
+      });
+      isCameraInitialized(true);
+      update();
+    } else {
+      print("Permission Denied");
+    }
+  }
+
+  initTFLite() async {
+    await Tflite.loadModel(
+      model: "assets/model.tflite",
+      labels: "assets/labels.txt",
+      isAsset: true,
+      numThreads: 1,
+    );
+  }
+
+  objectDetector(CameraImage image) async {
+    var detector = await Tflite.runModelOnFrame(
+      bytesList: image.planes.map((e) {
+        return e.bytes;
+      }).toList(),
+      asynch: true,
+      imageHeight: image.height,
+      imageWidth: image.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      numResults: 1,
+      threshold: 0.4,
+    );
+
+    if (detector != null) {
+
+       print("Result is $detector");
+
+    }
+
+  }
+}
--- lib/src/features/authentication/screens/camera/camera_screen.dart
+++ lib/src/features/authentication/screens/camera/camera_screen.dart
@@ -2,9 +2,12 @@ import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:fyp/src/features/authentication/controllers/camera_controller.dart';
+import 'package:get/get.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+
 
 class CameraScreen extends StatefulWidget {
   @override
@@ -12,76 +15,16 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
-  CameraController? _controller;
-  List<CameraDescription>? _cameras;
-  bool _isCameraInitialized = false;
-  Timer? _timer;
-
-  @override
-  void initState() {
-    super.initState();
-    _initCamera();
-  }
-
-  Future<void> _initCamera() async {
-    _cameras = await availableCameras();
-    if (_cameras!.isNotEmpty) {
-      _controller = CameraController(_cameras![1], ResolutionPreset.max);
-      _controller!.initialize().then((_) {
-        if (!mounted) {
-          return;
-        }
-        setState(() => _isCameraInitialized = true);
-        _timer = Timer(const Duration(seconds: 3), () => _captureAndSavePhoto());
-      });
-    }
-  }
-
-  Future<void> _captureAndSavePhoto() async {
-    if (_controller == null || !_controller!.value.isInitialized) {
-      return;
-    }
-
-    final XFile file = await _controller!.takePicture();
-    final Directory appDirectory = await getApplicationDocumentsDirectory();
-    final String picturePath = '${appDirectory.path}/${DateTime.now().toIso8601String()}.png';
-    final File pictureFile = File(picturePath);
-
-    await pictureFile.writeAsBytes(await file.readAsBytes());
-
-    // Save the photo to the device's gallery.
-    final result = await ImageGallerySaver.saveFile(picturePath);
-
-    if (result != null) {
-      // Show a message that the picture is taken.
-      Fluttertoast.showToast(
-        msg: "Picture Taken",
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.BOTTOM,
-        backgroundColor: Colors.black,
-        textColor: Colors.white,
-      );
-    }
-  }
-
-  @override
-  void dispose() {
-    _controller?.dispose();
-    _timer?.cancel();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    if (!_isCameraInitialized) {
-      return const Scaffold(
-        body: Center(child: CircularProgressIndicator()),
-      );
-    }
-
     return Scaffold(
-      appBar: AppBar(title: const Text('Camera')),
-      body: CameraPreview(_controller!), // Display the camera preview.
+      body: GetBuilder<ScanController>(
+        init: ScanController(),
+        builder: (controller){
+          return controller.isCameraInitialized.value? CameraPreview(controller.cameraController):const Center(child: Text("Loading Preview ..."));
+        },
+      ),
     );
   }
+
 }
