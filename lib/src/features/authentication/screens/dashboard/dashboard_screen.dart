6a8021095efaf55d7128943560dda202f1c91ab5
mhamz <m.hamzarasheed786@gmail.com>
2023-12-10 13:19:39 +0500

    Modifications in DashBoard

--- lib/src/features/authentication/screens/dashboard/dashboard_screen.dart
+++ lib/src/features/authentication/screens/dashboard/dashboard_screen.dart
@@ -22,57 +22,49 @@ class _DashBoardState extends State<DashBoard> {
     mediaSize = MediaQuery.of(context).size;
     return Container(
       decoration: BoxDecoration(
-          image: DecorationImage(
-        image: AssetImage(gWelcomeScreenImage),
-        fit: BoxFit.cover,
-        colorFilter: ColorFilter.mode(
-            gBoardPage2Color.withOpacity(0.2), BlendMode.dstATop),
-      )),
+        image: DecorationImage(
+          image: AssetImage(gWelcomeScreenImage),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            gBoardPage2Color.withOpacity(0.2),
+            BlendMode.dstATop,
+          ),
+        ),
+      ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: Stack(
           children: [
-            Positioned(top: 80, child: _buildTop()),
+            Positioned(top: 100, child: _buildTop()),
             Positioned(bottom: 0, child: _buildBottom()),
           ],
         ),
       ),
-      // child: Scaffold(
-      //   body: Container(
-      //     alignment: Alignment.center,
-      //     padding: const EdgeInsets.all(30.0),
-      //     child: IconButton(
-      //       onPressed: (){
-      //         Get.to(()=>const ProfileScreen());
-      //        // AuthenticationRepository.instance.logout();
-      //     },
-      //       icon:const Image(image: AssetImage(gGoogleLogoImage),),
-      //
-      //     ),
-      //   ),
-      // ),
     );
   }
 
   Widget _buildTop() {
     return SizedBox(
       width: mediaSize.width,
-      child: const Column(
+      child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Icon(
-            Icons.handshake,
-            size: 100,
-            color: Colors.white,
-          ),
-          Text(
-            "GESTURES",
-            style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 40,
-                letterSpacing: 2),
-          ),
+          // Icon(
+          //   Icons.handshake,
+          //   size: 100,
+          //   color: Colors.white,
+          // ),
+          // Text(
+          //   "GESTURES",
+          //   style: TextStyle(
+          //     color: Colors.white,
+          //     fontWeight: FontWeight.bold,
+          //     fontSize: 40,
+          //     letterSpacing: 2,
+          //   ),
+          // ),
+          SizedBox(height: 30,),
+          buildProfileImage(), // Use the buildProfileImage method here
         ],
       ),
     );
@@ -84,26 +76,47 @@ class _DashBoardState extends State<DashBoard> {
       child: Card(
         color: Colors.white,
         shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+        ),
         child: _buildForm(),
       ),
     );
   }
 
+  Widget buildProfileImage() => CircleAvatar(
+    radius: 70,
+    backgroundColor: Colors.grey,
+    backgroundImage: AssetImage(gWelcomeScreenImage),
+  );
+
   Widget _buildForm() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
+        SizedBox(height: 20,),
         Text(
-          "Welcome",
+          "Profile Name",
           style: TextStyle(
             color: Colors.black,
-            fontSize: 64,
+            fontSize: 30,
             fontWeight: FontWeight.bold,
           ),
         ),
-        SizedBox(height: 100),
+        SizedBox(height: 5),
+        Text(
+          "blah blah blah blah blah blah",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 10,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 50),
+        Container(width: MediaQuery.of(context).size.width,height: 3,color: Colors.black,),
+        SizedBox(height: 50),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -117,14 +130,17 @@ class _DashBoardState extends State<DashBoard> {
               ),
               child: SizedBox(
                 width: 50,
-                height: 80, // Increase the height as needed
+                height: 80,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Icon(Icons.library_books),
-                    SizedBox(width: 8),
+                    SizedBox(height: 8),
                     Text(
-                      "Library".toUpperCase(),
+                      "library".toUpperCase(),
+                      style: TextStyle(
+                        fontSize: 10,
+                      ),
                     ),
                   ],
                 ),
@@ -143,15 +159,18 @@ class _DashBoardState extends State<DashBoard> {
               ),
               child: SizedBox(
                 width: 50,
-                height: 80, // Increase the height as needed
+                height: 80,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Icon(Icons.library_books),
-                    SizedBox(width: 8),
+                    SizedBox(height: 8,),
                     Text(
-                      "Library".toUpperCase(),
-                    ),
+                      "Cmd".toUpperCase(),
+                      style: TextStyle(
+                        fontSize: 10,
+                      ),
+                    )
                   ],
                 ),
               ),
@@ -169,14 +188,17 @@ class _DashBoardState extends State<DashBoard> {
               ),
               child: SizedBox(
                 width: 50,
-                height: 80, // Increase the height as needed
+                height: 80,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Icon(Icons.library_books),
-                    SizedBox(width: 8),
+                    SizedBox(height: 8),
                     Text(
-                      "Library".toUpperCase(),
+                      "custom".toUpperCase(),
+                      style: TextStyle(
+                        fontSize: 10,
+                      ),
                     ),
                   ],
                 ),
@@ -191,3 +213,21 @@ class _DashBoardState extends State<DashBoard> {
     );
   }
 }
+
+
+
+
+// child: Scaffold(
+//   body: Container(
+//     alignment: Alignment.center,
+//     padding: const EdgeInsets.all(30.0),
+//     child: IconButton(
+//       onPressed: (){
+//         Get.to(()=>const ProfileScreen());
+//        // AuthenticationRepository.instance.logout();
+//     },
+//       icon:const Image(image: AssetImage(gGoogleLogoImage),),
+//
+//     ),
+//   ),
+// ),
